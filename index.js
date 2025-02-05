2df9eb4748c2f67da83bc3339901b7b2e698dac6
Vladimir Davydov <vdavydov@domclick.ru>
2025-02-05 15:31:42 +0300

    echo

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-const { TelegramClient } = require("telegram");
-const { StringSession } = require("telegram/sessions");
-const input = require("input");
+require('dotenv').config();
 
-const apiId = process.env.API_ID;
-const apiHash = process.env.API_HASH;
-const stringSession = new StringSession("");
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
-(async () => {
-  const client = new TelegramClient(stringSession, apiId, apiHash, {
-    connectionRetries: 5,
-  });
-
-  // Авторизация
-  await client.start({
-    phoneNumber: async () => await input.text("Введите номер телефона: "),
-    password: async () => await input.text("Введите пароль (если есть): "),
-    phoneCode: async () => await input.text("Введите код из Telegram: "),
-    onError: (err) => console.log(err),
-  });
-
-  console.log("Клиент подключен!");
-
-  // Сохранение сессии (чтобы не вводить код при каждом запуске)
-  console.log("Сохраненная сессия:", client.session.save());
-
-  await client.sendMessage("@username", { message: "Привет! Это тестовое сообщение." });
-
-  console.log("Сообщение отправлено!");
-})();
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, msg.text);
+})
