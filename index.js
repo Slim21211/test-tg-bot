71cd4f838b98088d6950c8b0c000031d3027bc5e
Vladimir Davydov <vdavydov@domclick.ru>
2025-02-05 18:21:33 +0300

    qqq

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Я скоро точно научусь что-то делать');
+bot.on('text', (msg) => {
+  if (msg.text === 'qqq') {
+    bot.sendMessage(msg.chat.id, msg.text)
+  } else {
+    bot.sendMessage(msg.chat.id, 'Я не знаю что ответить')
+  }
+  
 })
