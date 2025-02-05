7605d129b4564b9bbeeef96a602da72b2b552141
Vladimir Davydov <vdavydov@domclick.ru>
2025-02-05 19:10:53 +0300

    www

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 bot.on('text', (msg) => {
-  if (msg.text === 'qqq') {
+  if (msg.text === 'www') {
     bot.sendMessage(msg.chat.id, msg.text)
   } else {
     bot.sendMessage(msg.chat.id, 'Я пока не знаю что ответить на это')
