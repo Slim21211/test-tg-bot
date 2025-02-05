0792a0629023212b8227deebda6e7af3a4595ff9
Vladimir Davydov <vdavydov@domclick.ru>
2025-02-05 18:14:02 +0300

    new answer

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Я скоро научусь что-то делать');
+  bot.sendMessage(msg.chat.id, 'Я скоро точно научусь что-то делать');
 })
