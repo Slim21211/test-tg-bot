0370769cd2db3327e94cae1d230661d9de0f92ac
Vladimir Davydov <v.g.davydoff@gmail.com>
2025-02-05 23:36:35 +0300

    part of base education

---
+++ constants.js
@@ -0,0 +1,21 @@
+const emptyMessage = 'Материал на доработке\n\nДля возврата в начало нажмите /home'
+const declineSendText = 'Вы не можете делать рассылку в этом боте';
+const returnHomeText = 'Для возврата нажмите /home';
+const chooseChapterText = 'Выберите раздел:';
+const defaultText = 'Для возврата в начало нажмите /home\n\nЕсли у Вас возникли вопросы, ответы на которые Вы не нашли в этом боте, обратитесь в Отдел обучения и развития\n\nДля продолжения работы переключите клавиатуру на кнопки и выберите один из разделов ниже:'
+const chooseChapterWithReturnText = 'Выберите интересующий раздел\n\nДля возврата нажмите /home'
+const chooseChapterWithReturnAndBackText = 'Выберите формат обучающего материала:\n\nДля возврата в начало нажмите /home\n\nДля возврата к списку разделов нажмите /back'
+const linkToVideoText = 'Для просмотра видео перейдите по ссылке:'
+const returnBackText = 'Для возврата нажмите /back'
+
+module.exports = {
+  emptyMessage,
+  declineSendText,
+  returnHomeText,
+  chooseChapterText,
+  defaultText,
+  chooseChapterWithReturnText,
+  chooseChapterWithReturnAndBackText,
+  linkToVideoText,
+  returnBackText
+};
